053183efadd4674fe6cfc98e4e2aade2278d5aad
mikaeladev <100416544+mikaeladev@users.noreply.github.com>
2025-11-29 00:59:20 +0000

    add support for themes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,26 +1,59 @@
 #!/usr/bin/env bash
 
 config=$(toml get ./config.toml .)
-theme=$(echo "${config}" | jq -r .theme)
+metadata=$(echo "${config}" | jq -r .metadata)
+themes=$(echo "${config}" | jq -r .themes)
 cursors=$(echo "${config}" | jq -r .cursors)
 
 size=12
 scales=(1 2 3 4 5 6)
+theme=${1-"default"}
+
+if [[ $(echo "${themes}" | jq -r "keys[]") != *"${theme}"* ]]; then
+  echo "Invalid colour theme: '${theme}'" >&2
+  exit 1
+fi
+
+if [ "${theme}" != "default" ]; then
+  themeValues=$(echo "${themes}" | jq -r ".${theme}")
+  themePrimaryColour=$(echo "${themeValues}" | jq -r .primary)
+  themeSecondaryColour=$(echo "${themeValues}" | jq -r .secondary)
+  themeBorderColour=$(echo "${themeValues}" | jq -r .border)
+
+  defaultThemeValues=$(echo "${themes}" | jq -r .default)
+  defaultThemePrimaryColour=$(echo "${defaultThemeValues}" | jq -r .primary)
+  defaultThemeSecondaryColour=$(echo "${defaultThemeValues}" | jq -r .secondary)
+  defaultThemeBorderColour=$(echo "${defaultThemeValues}" | jq -r .border)
+fi
 
 rm -rf ./build ./dist
 mkdir -p ./build/configs ./dist/cursors ./dist/cursors_scalable
 
 cat >./dist/index.theme <<EOF
 [Icon Theme]
-Name=$(echo "${theme}" | jq -r ".name")
-Comment=$(echo "${theme}" | jq -r ".comment")
-Inherits=$(echo "${theme}" | jq -r ".inherits")
+Name=$(echo "${metadata}" | jq -r .name)
+Comment=$(echo "${metadata}" | jq -r .comment)
+Inherits=$(echo "${metadata}" | jq -r .inherits)
 EOF
 
 for scale in "${scales[@]}"; do
   scaledSize=$((size * scale))
   mkdir -p "./build/${scaledSize}x${scaledSize}"
 done
+
+applyThemeToSvg() {
+  local svgData=$1
+
+  if [ "${theme}" == "default" ]; then
+    echo "${svgData}"
+    return
+  fi
+
+  echo "${svgData}" | sed \
+    -e "s/${defaultThemePrimaryColour}/${themePrimaryColour}/g" \
+    -e "s/${defaultThemeSecondaryColour}/${themeSecondaryColour}/g" \
+    -e "s/${defaultThemeBorderColour}/${themeBorderColour}/g"
+}
 
 mkLinuxCursor() {
   local name=$1
@@ -40,10 +73,22 @@ mkLinuxCursor() {
       local buildDir="./build/${scaledDimensions}"
       local configFile="./build/configs/${name}.cursor"
 
+      local magickArgs=(
+        -scale "${scaledDimensions}"
+      )
+
+      if [ "${theme}" != "default" ]; then
+        magickArgs+=(
+          -fill "${themePrimaryColour}" -opaque "${defaultThemePrimaryColour}"
+          -fill "${themeSecondaryColour}" -opaque "${defaultThemeSecondaryColour}"
+          -fill "${themeBorderColour}" -opaque "${defaultThemeBorderColour}"
+        )
+      fi
+
       if [ "${delays}" == "null" ]; then
         local filename="${name}.png"
 
-        magick "./assets/${filename}" -scale "${scaledDimensions}" "${buildDir}/${filename}"
+        magick "./assets/${filename}" "${magickArgs[@]}" "${buildDir}/${filename}"
         echo "${scaledSize} ${scaledHotX} ${scaledHotY} ${buildDir}/${filename}" >>"${configFile}"
       else
         local currentFrame=1
@@ -51,7 +96,7 @@ mkLinuxCursor() {
         echo "${delays}" | while read -r delay; do
           local filename="${name}-${currentFrame}.png"
 
-          magick "./assets/${filename}" -scale "${scaledDimensions}" "${buildDir}/${filename}"
+          magick "./assets/${filename}" "${magickArgs[@]}" "${buildDir}/${filename}"
           echo "${scaledSize} ${scaledHotX} ${scaledHotY} ${buildDir}/${filename} ${delay}" >>"${configFile}"
 
           ((currentFrame++))
@@ -74,7 +119,7 @@ mkLinuxCursor() {
       local infilename="${name}.png"
       local outfilename="${name}.svg"
 
-      pixels2svg -o "${distDir}/${outfilename}" "./assets/${infilename}"
+      applyThemeToSvg "$(pixels2svg "./assets/${infilename}")" >"${distDir}/${outfilename}"
       echo "{ \"filename\": \"${outfilename}\", \"hotspot_x\": ${hotX}, \"hotspot_y\": ${hotY}, \"nominal_size\": ${size} }" >>"${metadataFile}"
     else
       local currentFrame=1
@@ -83,7 +128,7 @@ mkLinuxCursor() {
         local infilename="${name}-${currentFrame}.png"
         local outfilename="${name}-${currentFrame}.svg"
 
-        pixels2svg -o "${distDir}/${outfilename}" "./assets/${infilename}"
+        applyThemeToSvg "$(pixels2svg "./assets/${infilename}")" >"${distDir}/${outfilename}"
         echo "{ \"filename\": \"${outfilename}\", \"hotspot_x\": ${hotX}, \"hotspot_y\": ${hotY}, \"nominal_size\": ${size}, \"delay\": ${delay} }," >>"${metadataFile}"
 
         ((currentFrame++))
@@ -118,6 +163,6 @@ echo "${cursors}" | jq -r 'keys[]' | while read -r name; do
     aliases=$(echo "${aliases}" | jq -r ".[]")
   fi
 
-  echo "building '${name}' cursor"
-  mkLinuxCursor "${name}" "${hotX}" "${hotY}" "${delays}" "${aliases}" &
+  echo "building '${name}' cursor..."
+  mkLinuxCursor "${name}" "${hotX}" "${hotY}" "${delays}" "${aliases}"
 done
