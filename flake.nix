d102285ace51f6248e5a00d113b3f7a45355b23b
mikaeladev <100416544+mikaeladev@users.noreply.github.com>
2026-01-10 21:08:56 +0000

    configure treefmt

--- flake.nix
+++ flake.nix
@@ -4,12 +4,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+
       {
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
+        
         packages = rec {
           pixel-cursors = import ./package.nix pkgs;
           default = pixel-cursors;
---
+++ treefmt.nix
@@ -0,0 +1,22 @@
+{ ... }:
+
+let
+  lineWidth = 80;
+in
+
+{
+  projectRootFile = "flake.nix";
+
+  programs.nixfmt = {
+    enable = true;
+    strict = true;
+    indent = 2;
+    width = lineWidth;
+  };
+
+  programs.mdformat = {
+    enable = true;
+    settings.wrap = lineWidth;
+    plugins = ps: [ ps.mdformat-gfm ];
+  };
+}
